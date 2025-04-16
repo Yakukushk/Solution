a1f14e6fe5d3e1ca980c3ab5e11185b4e08584df
Danilka <daniilliubko@gmail.com>
2025-04-16 11:30:44 +0200

    Update Commit

--- nuxt-app/src/entities/note/lib/validators.ts
+++ nuxt-app/src/entities/note/lib/validators.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
 export const noteSchema = yup.object({
-  title: yup.string().required("Назва обов'язкова").max(100, 'Максимум 100 символів'),
-  content: yup.string().required("Контент обов'язковий").max(1000, 'Максимум 1000 символів'),
+  title: yup.string().trim().required("Назва обов'язкова").max(100, 'Максимум 100 символів'),
+  content: yup.string().trim().required("Контент обов'язковий").max(1000, 'Максимум 1000 символів'),
 });
